b7c8127ce32cdff97f88368f12fff6c4e6a945fe
DevRubia <78357146+DevRubia@users.noreply.github.com>
2023-04-16 22:10:20 +0300

    Update index.php

--- index.php
+++ index.php
@@ -30,19 +30,19 @@ body{
 <form class="container bg-white text-black center border border-warning">
   
 <div class="form-group">
-    <label for="exampleInputPassword1">Name</label>
+    <h4 for="exampleInputPassword1">Name</h4>
     <input type="text" class="form-control"  name="name" id="exampleInputPassword1" placeholder="Password">
   </div>
 
   <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
+    <h4 for="exampleInputEmail1">Email address</h4>
     <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
     <small id="emailHelp" class="form-text text-muted">your email is secure</small>
   </div>
   
   <div class="form-group form-check">
     <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+    <h4 class="form-check-label" for="exampleCheck1">Check me out</h4>
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
